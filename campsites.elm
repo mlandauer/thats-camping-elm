69921e37088a95477ab48532a55cf5833780036c
Matthew Landauer <mlandauer@gmail.com>
2017-01-13 09:23:26 +1100

    Extract function

--- campsites.elm
+++ campsites.elm
@@ -3,7 +3,10 @@ import Html.Attributes exposing (class)
 
 main =
   ul [class "campsite-list"]
-    [ li [] [text "Acacia Flats"]
-    , li [] [text "Alexanders Hut"]
-    , li [] [text "Apsley Falls"]
+    [ campsiteListItem "Acacia Flats"
+    , campsiteListItem "Alexanders Hut"
+    , campsiteListItem "Apsley Falls"
     ]
+
+campsiteListItem name =
+  li [] [text name]
