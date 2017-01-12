67ef87333dfde116d9f2474316cea15a771dd62f
Matthew Landauer <mlandauer@gmail.com>
2017-01-13 09:58:30 +1100

    Use elm-format automatically now

--- campsites.elm
+++ campsites.elm
@@ -1,16 +1,30 @@
+module Main exposing (..)
+
 import Html exposing (li, text, ul, Html)
 import Html.Attributes exposing (class)
 
-type alias Campsite = {name: String}
 
-c1 = Campsite "Acacia Flats"
-c2 = Campsite "Alexanders Hut"
-c3 = Campsite "Apsley Falls"
+type alias Campsite =
+    { name : String }
+
+
+c1 =
+    Campsite "Acacia Flats"
+
+
+c2 =
+    Campsite "Alexanders Hut"
+
+
+c3 =
+    Campsite "Apsley Falls"
+
 
 main =
-  ul [class "campsite-list"]
-    (List.map campsiteListItem [c1, c2, c3])
+    ul [ class "campsite-list" ]
+        (List.map campsiteListItem [ c1, c2, c3 ])
+
 
 campsiteListItem : Campsite -> Html msg
 campsiteListItem campsite =
-  li [] [text campsite.name]
+    li [] [ text campsite.name ]
