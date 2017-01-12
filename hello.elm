17956c28b947dc92e1557c51c78c9d032f2fcc41
Matthew Landauer <mlandauer@gmail.com>
2017-01-13 08:54:35 +1100

    Show a list of campsites

--- hello.elm
+++ hello.elm
@@ -1,4 +1,9 @@
-import Html exposing (text)
+import Html exposing (li, text, ul)
+import Html.Attributes exposing (class)
 
 main =
-  text "Hello, World!"
+  ul [class "campsite-list"]
+    [ li [] [text "Acacia Flats"]
+    , li [] [text "Alexanders Hut"]
+    , li [] [text "Apsley Falls"]
+    ]
