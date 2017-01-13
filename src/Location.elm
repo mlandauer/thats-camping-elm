062f7aaf2d486a6ef40e1481c9b0b475462b7743
Matthew Landauer <mlandauer@gmail.com>
2017-01-14 06:22:48 +1100

    Rename variables to have more intuitive meaning because operations are signed

--- src/Location.elm
+++ src/Location.elm
@@ -22,25 +22,25 @@ degrees location =
 
 
 distanceInMetres : Location -> Location -> Float
-distanceInMetres position1 position2 =
+distanceInMetres to from =
     let
-        p1 =
-            degrees position1
+        t =
+            degrees to
 
-        p2 =
-            degrees position2
+        f =
+            degrees from
 
         r =
             6371000
 
         dLat =
-            p2.latitude - p1.latitude
+            f.latitude - t.latitude
 
         dLon =
-            p2.longitude - p1.longitude
+            f.longitude - t.longitude
 
         a =
-            sin (dLat / 2) * sin (dLat / 2) + cos (p1.latitude) * cos (p2.latitude) * sin (dLon / 2) * sin (dLon / 2)
+            sin (dLat / 2) * sin (dLat / 2) + cos (t.latitude) * cos (f.latitude) * sin (dLon / 2) * sin (dLon / 2)
 
         c =
             2 * atan2 (sqrt a) (sqrt (1 - a))
@@ -49,22 +49,22 @@ distanceInMetres position1 position2 =
 
 
 bearingInDegrees : Location -> Location -> Float
-bearingInDegrees position1 position2 =
+bearingInDegrees to from =
     let
-        p1 =
-            degrees position1
+        t =
+            degrees to
 
-        p2 =
-            degrees position2
+        f =
+            degrees from
 
         dLon =
-            p1.longitude - p2.longitude
+            t.longitude - f.longitude
 
         y =
-            sin (dLon) * cos (p1.latitude)
+            sin (dLon) * cos (t.latitude)
 
         x =
-            cos (p2.latitude) * sin (p1.latitude) - sin (p2.latitude) * cos (p1.latitude) * cos (dLon)
+            cos (f.latitude) * sin (t.latitude) - sin (f.latitude) * cos (t.latitude) * cos (dLon)
 
         bearing =
             (atan2 y x) / pi * 180
