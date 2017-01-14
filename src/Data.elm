d56eee76a143d25c2be4fcf44ca5e8e5bf318d51
Matthew Landauer <mlandauer@gmail.com>
2017-01-14 19:42:46 +1100

    Handle location data missing in json

--- src/Data.elm
+++ src/Data.elm
@@ -4,8 +4,10 @@ import Json.Decode
 import Location exposing (Location)
 
 
-locationDecoder : Json.Decode.Decoder Location
+locationDecoder : Json.Decode.Decoder (Maybe Location)
 locationDecoder =
-    Json.Decode.map2 Location
-        (Json.Decode.field "latitude" Json.Decode.float)
-        (Json.Decode.field "longitude" Json.Decode.float)
+    Json.Decode.maybe
+        (Json.Decode.map2 Location
+            (Json.Decode.field "latitude" Json.Decode.float)
+            (Json.Decode.field "longitude" Json.Decode.float)
+        )
--- tests/TestsData.elm
+++ tests/TestsData.elm
@@ -4,14 +4,32 @@ import Test exposing (..)
 import Expect
 import Json.Decode
 import Data
+import Location exposing (Location)
 
 
 all : Test
 all =
     describe "Data"
         [ describe "locationDecoder"
-            [ test "example" <|
+            [ test "location data is present" <|
                 \() ->
-                    Expect.equal (Ok { latitude = -33, longitude = 150 }) (Json.Decode.decodeString Data.locationDecoder """{ "latitude": -33, "longitude": 150 }""")
+                    let
+                        json =
+                            """{ "latitude": -33, "longitude": 150 }"""
+
+                        expected =
+                            Ok (Just (Location -33 150))
+                    in
+                        Expect.equal expected (Json.Decode.decodeString Data.locationDecoder json)
+            , test "location data is absent" <|
+                \() ->
+                    let
+                        json =
+                            "{}"
+
+                        expected =
+                            Ok (Nothing)
+                    in
+                        Expect.equal expected (Json.Decode.decodeString Data.locationDecoder json)
             ]
         ]
