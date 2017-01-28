8e7b3e534e0c6c92ccea72b63956935d71327e87
Matthew Landauer <mlandauer@gmail.com>
2017-01-29 08:59:33 +1100

    Wrap about page content in container

--- src/Pages/About/View.elm
+++ src/Pages/About/View.elm
@@ -9,28 +9,30 @@ view =
     div [ class "campsite-list" ]
         [ navBar "About" True False
         , div [ class "content" ]
-            [ h2 [] [ text "About That's Camping" ]
-            , p [] [ text "Find campsites near you in New South Wales, Australia. It covers camping on public, common land such as National Parks, State Forests and Local Council land." ]
-            , p []
-                [ text "It works "
-                , strong [] [ text "completely offline" ]
-                , text ", even when you're far far away from a mobile phone tower. When does that ever happen while camping?"
-                ]
-            , p []
-                [ text "Made by "
-                , a [ href "https://twitter.com/matthewlandauer" ] [ text "Matthew Landauer" ]
-                , text " based on an iOS app. It's free and "
-                , a [ href "https://github.com/mlandauer/thats-camping-elm" ] [ text "open source" ]
-                , text " because that's the way it ought to be."
-                ]
-              -- TODO: Show current version of the app here
-            , h2 [] [ text "Things you might want to do" ]
-            , p []
-                [ a [ href "https://github.com/mlandauer/thats-camping-elm/issues" ]
-                    [ text "Suggest a "
-                    , strong [] [ text "feature" ]
-                    , text " or report an "
-                    , strong [] [ text "issue" ]
+            [ div [ class "container" ]
+                [ h2 [] [ text "About That's Camping" ]
+                , p [] [ text "Find campsites near you in New South Wales, Australia. It covers camping on public, common land such as National Parks, State Forests and Local Council land." ]
+                , p []
+                    [ text "It works "
+                    , strong [] [ text "completely offline" ]
+                    , text ", even when you're far far away from a mobile phone tower. When does that ever happen while camping?"
+                    ]
+                , p []
+                    [ text "Made by "
+                    , a [ href "https://twitter.com/matthewlandauer" ] [ text "Matthew Landauer" ]
+                    , text " based on an iOS app. It's free and "
+                    , a [ href "https://github.com/mlandauer/thats-camping-elm" ] [ text "open source" ]
+                    , text " because that's the way it ought to be."
+                    ]
+                  -- TODO: Show current version of the app here
+                , h2 [] [ text "Things you might want to do" ]
+                , p []
+                    [ a [ href "https://github.com/mlandauer/thats-camping-elm/issues" ]
+                        [ text "Suggest a "
+                        , strong [] [ text "feature" ]
+                        , text " or report an "
+                        , strong [] [ text "issue" ]
+                        ]
                     ]
                 ]
             ]
