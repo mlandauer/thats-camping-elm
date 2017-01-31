f307959f99d552078b9a99c2e6e7b2898d15f476
Matthew Landauer <mlandauer@gmail.com>
2017-01-31 16:29:18 +1100

    Now syncs with cloudant

--- src/index.js
+++ src/index.js
@@ -18,6 +18,35 @@ var app = Elm.App.embed(node);
 
 var db = new PouchDB('thats-camping');
 
+var remoteDb = new PouchDB('https://mlandauer.cloudant.com/thats-camping', {
+  auth: {
+    // We're just using these tokens to get access to the remote database
+    // temporarily.
+    // TODO: Generate new tokens and remove from source code
+    username: 'cirtionewessinstroonheen',
+    password: '5e5308b9016a700156164970fbb346f9ede08e8e'
+  }
+});
+
+db.sync(remoteDb).on('change', function (info) {
+  console.log("sync change:", info);
+}).on('paused', function (err) {
+  // replication paused (e.g. replication up to date, user went offline)
+  console.log("sync paused:", err);
+}).on('active', function () {
+  // replicate resumed (e.g. new changes replicating, user went back online)
+  console.log("sync active");
+}).on('denied', function (err) {
+  // a document failed to replicate (e.g. due to permissions)
+  console.log("sync denied:", err);
+}).on('complete', function (info) {
+  // handle complete
+  console.log("sync complete:", info);
+}).on('error', function (err) {
+  // handle error
+  console.log("sync error:", err);
+});
+
 db.changes({
   live: true,
   include_docs: true
