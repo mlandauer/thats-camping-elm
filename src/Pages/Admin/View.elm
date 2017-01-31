487ffacdf8ec7fdc49136ec0d3502f9b37ac92fb
Matthew Landauer <mlandauer@gmail.com>
2017-01-31 14:52:27 +1100

    Load one campsite

--- src/Pages/Admin/View.elm
+++ src/Pages/Admin/View.elm
@@ -14,6 +14,7 @@ view model =
             [ p [] [ text "This will become the admin page to experiment with pouchdb" ]
             , button [ class "btn btn-default", onClick AddData ] [ text "Add some data to the model" ]
             , button [ class "btn btn-default", onClick Destroy ] [ text "Destroy local database" ]
+            , button [ class "btn btn-default", onClick LoadData ] [ text "Load local database campsite data from Github" ]
             , p [] [ text (Maybe.withDefault "" model.text) ]
             ]
         ]
