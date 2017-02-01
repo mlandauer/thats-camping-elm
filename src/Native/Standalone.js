61f439b73763801221fd10833ffbb80e188cd4fb
Matthew Landauer <mlandauer@gmail.com>
2017-02-01 14:12:57 +1100

    Logic was wrong way around

--- src/Native/Standalone.js
+++ src/Native/Standalone.js
@@ -3,7 +3,7 @@ var _user$project$Native_Standalone = function()
 
 var standalone = _elm_lang$core$Native_Scheduler.nativeBinding(function(callback)	{
 	callback(_elm_lang$core$Native_Scheduler.succeed(("standalone" in window.navigator) &&
-    !window.navigator.standalone));
+    window.navigator.standalone));
 });
 
 return {
