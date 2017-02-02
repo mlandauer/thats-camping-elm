61d95f737932669c6028bcbb777f0a00c7eee945
Matthew Landauer <mlandauer@gmail.com>
2017-02-03 08:12:04 +1100

    Improve admin page explanatory wording

--- src/elm/Pages/Admin/View.elm
+++ src/elm/Pages/Admin/View.elm
@@ -11,7 +11,8 @@ view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ div [ class "container" ]
-            [ p [] [ text "This will become the admin page to experiment with pouchdb" ]
+            [ h2 [] [ text "Database admin page" ]
+            , p [] [ text "Here you can do actions on the local PouchDB database" ]
             , button [ class "btn btn-default", onClick Destroy ] [ text "Destroy local database" ]
             , button [ class "btn btn-default", onClick LoadData ] [ text "Load local database campsite data from Github" ]
             , p [] [ text (Maybe.withDefault "" model.text) ]
