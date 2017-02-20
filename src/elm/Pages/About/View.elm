2352721a17832eb0958e446ecb4ae33ba1ffc3a0
Matthew Landauer <mlandauer@gmail.com>
2017-02-21 10:46:51 +1100

    Convert about page to markdown

--- src/elm/Pages/About/View.elm
+++ src/elm/Pages/About/View.elm
@@ -4,6 +4,8 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import App.ViewHelpers exposing (navBar)
 import App.Update exposing (Msg)
+import Markdown
+import Regex
 
 
 view : String -> Html Msg
@@ -11,34 +13,29 @@ view version =
     div [ class "campsite-list" ]
         [ navBar "About" True False
         , div [ class "content" ]
-            [ div [ class "container" ]
-                [ h2 [] [ text "About That's Camping" ]
-                , p [] [ text "Find campsites near you in New South Wales, Australia. It covers camping on public, common land such as National Parks, State Forests and Local Council land." ]
-                , p []
-                    [ text "It works "
-                    , strong [] [ text "completely offline" ]
-                    , text ", even when you're far far away from a mobile phone tower. When does that ever happen while camping?"
-                    ]
-                , p []
-                    [ text "Made by "
-                    , a [ href "https://twitter.com/matthewlandauer" ] [ text "Matthew Landauer" ]
-                    , text " based on an iOS app. It's free and "
-                    , a [ href "https://github.com/mlandauer/thats-camping-elm" ] [ text "open source" ]
-                    , text " because that's the way it ought to be."
-                    ]
-                , p []
-                    [ text "You're currently using version "
-                    , a [ href ("https://github.com/mlandauer/thats-camping-elm/commit/" ++ version) ] [ text version ]
-                    ]
-                , h2 [] [ text "Things you might want to do" ]
-                , p []
-                    [ a [ href "https://github.com/mlandauer/thats-camping-elm/issues" ]
-                        [ text "Suggest a "
-                        , strong [] [ text "feature" ]
-                        , text " or report an "
-                        , strong [] [ text "issue" ]
-                        ]
-                    ]
-                ]
+            [ let
+                content =
+                    """
+## About That's Camping
+
+Find campsites near you in New South Wales, Australia. It covers camping on
+public, common land such as National Parks, State Forests and Local Council
+land.
+
+It works **completely offline**, even when you're far far away from a mobile phone tower. When does that ever happen while camping?
+
+Made by [Matthew Landauer](https://twitter.com/matthewlandauer) based on an iOS
+app. It's free and [open source](https://github.com/mlandauer/thats-camping-elm)
+because that's the way it ought to be.
+
+You're currently using version [{{version}}](https://github.com/mlandauer/thats-camping-elm/commit/{{version}}).
+
+## Things you might want to do
+
+[Suggest a **feature** or report an **issue**](https://github.com/mlandauer/thats-camping-elm/issues)
+"""
+              in
+                -- Doing poor man's string interpolation here
+                Markdown.toHtml [ class "container" ] (Regex.replace Regex.All (Regex.regex "{{version}}") (\_ -> version) content)
             ]
         ]
