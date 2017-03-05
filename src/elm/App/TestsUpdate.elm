7338ca4fe545c6773a0a0cc94abd3258e1b50768
Matthew Landauer <mlandauer@gmail.com>
2017-03-06 10:58:22 +1100

    Rewrite with pipe syntax

--- src/elm/App/TestsUpdate.elm
+++ src/elm/App/TestsUpdate.elm
@@ -8,24 +8,23 @@ import App.Model exposing (..)
 
 all : Test
 all =
-    describe "App.Update.updateWithMap"
-        [ describe "ChangePage"
-            [ test "sets the page" <|
-                \() ->
-                    let
-                        model =
-                            App.Update.initModel
-                                { version = "1.0"
-                                , standalone = False
-                                , starredCampsites = Nothing
-                                , online = True
-                                , location = Nothing
-                                }
-                    in
-                        Expect.equal (CampsitesPage Map) <|
-                            .page <|
-                                Tuple.first <|
-                                    App.Update.updateWithMap (ChangePage (CampsitesPage Map))
-                                        model
+    let
+        defaultFlags =
+            { version = "1.0"
+            , standalone = False
+            , starredCampsites = Nothing
+            , online = True
+            , location = Nothing
+            }
+    in
+        describe "App.Update.updateWithMap"
+            [ describe "ChangePage"
+                [ test "sets the page" <|
+                    \() ->
+                        App.Update.initModel defaultFlags
+                            |> App.Update.updateWithMap (ChangePage (CampsitesPage Map))
+                            |> Tuple.first
+                            |> .page
+                            |> Expect.equal (CampsitesPage Map)
+                ]
             ]
-        ]
