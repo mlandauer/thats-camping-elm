148f50a7cdfb5faf5f86cedbbd2195916a970eb9
Matthew Landauer <mlandauer@gmail.com>
2017-03-14 13:26:16 +1100

    Get rid of initial debugging output

--- src/server.js
+++ src/server.js
@@ -2,11 +2,9 @@ var Elm = require('./elm/Server.elm');
 
 var app = Elm.Server.worker();
 
-console.log("Hello world!");
-
 // Send a request to elm (via ports)
 app.ports.request.send(null);
 
 app.ports.response.subscribe(function(response) {
-  console.log("response", response);
+  console.log("response:", response);
 });
