4f71ec345cac6b77574eee86d80aa0f67fa02676
Matthew Landauer <mlandauer@gmail.com>
2017-03-15 11:41:40 +1100

    Now client javascript takes over

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,70 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var gitRevisionPlugin = new GitRevisionPlugin();
 
-module.exports = {
+// TODO: Remove duplication between clientConfig and serverConfig
+
+clientConfig = {
   entry: {
     app: [
+      './src/index.js'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname + '/docs'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: "css-loader?sourceMap",
+        })
+      },
+      {
+        test:    /\.(html|png)$/,
+        use:  'file-loader?name=[name].[ext]',
+      },
+      {
+        test:    /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use:  'elm-webpack-loader?verbose=true&warn=true',
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'file-loader',
+      },
+    ],
+
+    noParse: /\.elm$/,
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    inline: true,
+    stats: { colors: true },
+  },
+
+  plugins: [
+    new AppCachePlugin({cache: ["/", "/campsites"], exclude: ['index.html']}),
+    new GitRevisionPlugin(),
+    new webpack.DefinePlugin({
+      'VERSION': JSON.stringify(gitRevisionPlugin.version())
+    }),
+    new ExtractTextPlugin('app.css')
+  ]
+};
+
+serverConfig = {
+  entry: {
+    server: [
       './src/server.js'
     ]
   },
@@ -67,3 +128,5 @@ module.exports = {
     new ExtractTextPlugin('app.css')
   ]
 };
+
+module.exports = [clientConfig, serverConfig];
