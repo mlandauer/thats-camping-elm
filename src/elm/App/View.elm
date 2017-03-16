53dc8efbd25103f234e0479e218783ab83a2fc55
Matthew Landauer <mlandauer@gmail.com>
2017-03-16 16:53:23 +1100

    Use link for back button now

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -69,42 +69,60 @@ view model =
 
 navView : Model -> Html Msg
 navView model =
-    case model.page of
-        Nothing ->
-            text ""
+    let
+        -- If there is no previous page just go to the home page
+        backPage =
+            Maybe.withDefault (CampsitesPage List) model.previousPage
+    in
+        case model.page of
+            Nothing ->
+                text ""
 
-        Just (CampsitesPage displayType) ->
-            if Dict.isEmpty model.campsites then
-                navBar "" { back = False, about = False }
-            else
-                navBar "Camping near you" { back = False, about = True }
+            Just (CampsitesPage displayType) ->
+                if Dict.isEmpty model.campsites then
+                    navBar "" { back = Nothing, about = False }
+                else
+                    navBar "Camping near you" { back = Nothing, about = True }
 
-        Just (CampsitePage id) ->
-            case Dict.get id model.campsites of
-                Just campsite ->
-                    navBar campsite.name.short { back = True, about = True }
+            Just (CampsitePage id) ->
+                case Dict.get id model.campsites of
+                    Just campsite ->
+                        navBar campsite.name.short { back = Just backPage, about = True }
 
-                Nothing ->
-                    navBar "404" { back = True, about = False }
+                    Nothing ->
+                        navBar "404" { back = Just backPage, about = False }
 
-        Just AboutPage ->
-            navBar "About" { back = True, about = False }
+            Just AboutPage ->
+                navBar "About" { back = Just backPage, about = False }
 
-        Just (TourPage id) ->
-            navBar "" { back = (id /= Start), about = False }
+            Just (TourPage id) ->
+                navBar ""
+                    { back =
+                        (if id /= Start then
+                            Just backPage
+                         else
+                            Nothing
+                        )
+                    , about = False
+                    }
 
-        Just AdminPage ->
-            navBar "Database admin" { back = True, about = False }
+            Just AdminPage ->
+                navBar "Database admin" { back = Just backPage, about = False }
 
-        Just UnknownPage ->
-            navBar "404" { back = False, about = False }
+            Just UnknownPage ->
+                navBar "404" { back = Just backPage, about = False }
 
 
-navBar : String -> { back : Bool, about : Bool } -> Html Msg
+navBar : String -> { back : Maybe Page, about : Bool } -> Html Msg
 navBar title { back, about } =
     nav [ class "navbar navbar-default navbar-fixed-top" ]
         [ div [ class "container" ]
-            ([ show back backButton
+            ([ case back of
+                Just page ->
+                    backButton page
+
+                Nothing ->
+                    text ""
              , show about aboutButton
              , h1 [] [ text title ]
              ]
@@ -120,9 +138,11 @@ show s html =
         text ""
 
 
-backButton : Html Msg
-backButton =
-    button [ onClick PageBack, class "btn btn-link navbar-link navbar-text pull-left" ]
+backButton : Page -> Html Msg
+backButton page =
+    App.ViewHelpers.link ChangePage
+        page
+        [ class "btn btn-link navbar-link navbar-text pull-left" ]
         [ App.ViewHelpers.glyphicon "menu-left" ]
 
 
