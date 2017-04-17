2492845606c2447f520c1d4d8ed3a30868710b3c
Matthew Landauer <mlandauer@gmail.com>
2017-04-17 12:23:06 +1000

    Get npm run dev working again. Fixes #107

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ require('bootstrap/dist/css/bootstrap.css');
 require('./assets/styles/style.css');
 // require('font-awesome/css/font-awesome.css');
 
+// In production don't generate index.html because that is rendered
+// dynamically but we still want the static thing in development because
+// it makes the development experience easier and faster
+if (process.env.NODE_ENV !== 'production') {
+  require('./index.html');
+}
+
 // Required for google webmaster tools site verification (and it needs to stay)
 require('./assets/html/google6d01d0a1bce5c547.html');
 require('./assets/images/apple-touch-icon.png');
